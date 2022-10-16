1f4988c81695715ca97c3ebee409f9ebdc7dc83f
hiroshiimaizumi <hiroshiimaizumi0611@gmail.com>
2022-10-16 16:40:46 +0900

    connect customer to fraud

--- customer/src/main/java/com/microservices/customer/CustomerService.java
+++ customer/src/main/java/com/microservices/customer/CustomerService.java
@@ -1,9 +1,18 @@
 package com.microservices.customer;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
-public record CustomerService(CustomerRepository customerRepository) {
+public class CustomerService {
+    private final CustomerRepository customerRepository;
+    private final RestTemplate restTemplate;
+
+    public CustomerService(CustomerRepository customerRepository, RestTemplate restTemplate) {
+        this.customerRepository = customerRepository;
+        this.restTemplate = restTemplate;
+    }
+
     public void registerCustomer(CustomerRegistrationRequest request) {
         Customer customer = Customer.builder()
                 .firstName(request.firstName())
@@ -12,6 +21,17 @@ public record CustomerService(CustomerRepository customerRepository) {
                 .build();
         // todo: check if email valid
         // todo: check if email not taken
-        customerRepository.save(customer);
+        customerRepository.saveAndFlush(customer);
+        // todo: check if fraudster
+        FraudCheckResponse fraudCheckResponse = restTemplate.getForObject(
+                "http://localhost:8081/api/v1/fraud-check/{customerId}",
+                FraudCheckResponse.class,
+                customer.getId()
+        );
+
+        if (fraudCheckResponse.isFraudster()){
+            throw new IllegalStateException("fraudster");
+        }
+        // todo: send notification
     }
 }
--- fraud/src/main/java/com/microservices/fraud/FraudController.java
+++ fraud/src/main/java/com/microservices/fraud/FraudController.java
@@ -1,7 +1,8 @@
 package com.microservices.fraud;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
-
+@Slf4j
 @RestController
 @RequestMapping("api/v1/fraud-check")
 public class FraudController {
@@ -16,6 +17,7 @@ public class FraudController {
     public FraudCheckResponse isFraudster(@PathVariable("customerId") Integer customerId) {
         boolean isFraudulentCustomer = fraudCheckService.isFraudulentCustomer(customerId);
 
+        log.info(" fraud check request for customer {}", customerId);
         return new FraudCheckResponse(isFraudulentCustomer);
     }
 
